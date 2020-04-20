ec36d643fe4d5ea695b5bff5b205ae0dd237f3e5
yashsinha490 <yashsinha4909@gmail.com>
2020-04-21 02:56:12 +0530

    added the search button so that the users can search the city of their choice

--- weather/views.py
+++ weather/views.py
@@ -1,13 +1,12 @@
 import requests
 from django.shortcuts import render
 from .models import weath
-
 def wethinf(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=86feeef11ba4a5d3bdfbe011b916a7a3&units=metric'
-
-
-    allweath = []
-    for city in weath.objects.all():
+    if request.method != 'POST':
+        return render(request, 'weather/home.html')
+    else:
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=86feeef11ba4a5d3bdfbe011b916a7a3&units=metric'
+        city = request.POST['text']
         r = requests.get(url.format(city)).json()
 
         curr_weath={
@@ -15,9 +14,5 @@ def wethinf(request):
             'temperature':r['main']['temp'],
             'description':r['weather'][0]['description'],
             'icon':r['weather'][0]['icon']
-        }
-        allweath.append(curr_weath)
-
-
-    print(allweath)
-    return render(request, 'weather/home.html', {'allweath':allweath} )
+            }
+        return render(request, 'weather/home.html', {'curr_weath': curr_weath})
