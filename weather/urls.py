8fd0b6873489ce537b319faaea90bff844f18ed7
yashsinha490 <yashsinha4909@gmail.com>
2020-04-22 03:11:24 +0530

    added the feature for the users to now get the information in their own language, and did some visual modifications

--- weather/urls.py
+++ weather/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
        path('', views.home, name = "home"),
-       path('info', views.wethinf, name = "wethome")
+       path('info', views.wethinf, name = "wethome"),
+       path('lang', views.lang, name = "lang"),
+       path('wetlang', views.wetlang, name = "wetlang"),
+       path('langinfo', views.langinfo, name = "langinfo")
 ]
--- weather/views.py
+++ weather/views.py
@@ -17,3 +17,22 @@ def wethinf(request):
         'icon':r['weather'][0]['icon']
         }
      return render(request, 'weather/weathinfo.html', {'curr_weath': curr_weath})
+
+def lang(request):
+    return render(request, 'weather/lang.html')
+
+def wetlang(request):
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=86feeef11ba4a5d3bdfbe011b916a7a3&units=metric&lang={}'
+        city = request.POST['text']
+        lang = request.POST['lang']
+        r = requests.get(url.format(city,lang)).json()
+        curr_weath={
+           'city':city,
+           'temperature':r['main']['temp'],
+           'description':r['weather'][0]['description'],
+           'icon':r['weather'][0]['icon']
+           }
+        return render(request, 'weather/wetlang.html', {'curr_weath': curr_weath})
+
+def langinfo(request):
+    return render(request, 'weather/langinfo.html')
